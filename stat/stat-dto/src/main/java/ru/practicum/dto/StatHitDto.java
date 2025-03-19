9fdb690fac55d5e6bcfcc122cdfcdadb2b84b0a0
m.kiselev <sanwiraight@gmail.com>
2025-03-19 20:20:13 +0300

    [Sprint 17][stat-server] Implement start-server

--- stat/stat-dto/src/main/java/ru/practicum/dto/StatHitDto.java
+++ stat/stat-dto/src/main/java/ru/practicum/dto/StatHitDto.java
@@ -1,6 +1,13 @@
 package ru.practicum.dto;
 
-import lombok.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDateTime;
@@ -11,15 +18,18 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class StatHitDto {
-
     Long id;
 
+    @NotBlank
     String app;
 
+    @NotBlank
     String uri;
 
+    @NotBlank
     String ip;
 
+    @PastOrPresent
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     LocalDateTime timestamp;
-
 }
