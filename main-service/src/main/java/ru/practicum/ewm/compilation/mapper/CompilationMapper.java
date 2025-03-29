bdef8c0dbc5556a034b2a9d01afaf9cbfabb9d80
Sergey Andreev <siarhe1.andreeff@yandex.kz>
2025-03-29 19:20:28 +0300

    Add Compilation

--- main-service/src/main/java/ru/practicum/ewm/compilation/mapper/CompilationMapper.java
+++ main-service/src/main/java/ru/practicum/ewm/compilation/mapper/CompilationMapper.java
@@ -3,46 +3,39 @@ package ru.practicum.ewm.compilation.mapper;
 import ru.practicum.ewm.compilation.dto.CompilationDto;
 import ru.practicum.ewm.compilation.dto.NewCompilationDto;
 import ru.practicum.ewm.compilation.model.Compilation;
+import ru.practicum.ewm.events.dto.EventShortDto;
+import ru.practicum.ewm.events.model.Event;
+
+import java.util.List;
 
 public class CompilationMapper {
 
-    public static CompilationDto toCompilationDto(Compilation compilation) {
+    public static CompilationDto toCompilationDto(Compilation compilation, List<EventShortDto> EventShortDtos) {
 
         return CompilationDto.builder()
                 .id(compilation.getId())
                 .title(compilation.getTitle())
                 .pinned(compilation.isPinned())
-//                .events(compilation.getEvents() != null ?
-//                        compilation.getEvents().stream()
-//                                .map(EventMapper::toEventShortDto)
-//                                .collect(Collectors.toList()) : null)
+                .events(EventShortDtos)
                 .build();
     }
 
-    public static Compilation toCompilation(CompilationDto compilationDto) {
+    public static Compilation toCompilation(CompilationDto compilationDto, List<Event> events) {
 
         return Compilation.builder()
                 .id(compilationDto.getId())
                 .title(compilationDto.getTitle())
                 .pinned(compilationDto.isPinned())
-                // TODO Implement EventShortDto -> Event
-//                .events(compilationDto.getEvents() != null ?
-//                        compilationDto.getEvents().stream()
-//                                .map(EventMapper::toEventEntity)
-//                                .collect(Collectors.toList()) : null)
+                .events(events)
                 .build();
     }
 
-    public static Compilation toCompilationEntity(NewCompilationDto newCompilationDto) {
+    public static Compilation toCompilationEntity(NewCompilationDto newCompilationDto, List<Event> events) {
 
         return Compilation.builder()
                 .title(newCompilationDto.getTitle())
                 .pinned(newCompilationDto.isPinned())
-                // TODO Implement Long -> Event or other fetching data for events
-//                .events(newCompilationDto.getEvents() != null ?
-//                        newCompilationDto.getEvents().stream()
-//                                .map(EventMapper::toEventEntity)
-//                                .collect(Collectors.toList()) : null)
+                .events(events)
                 .build();
     }
 }
